c8374576d12fc65d3b93cabf79bb8051d123fc05
hakunagi <hakunagi.rei@gmail.com>
2021-10-11 01:10:25 +0900

    まあまあ

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,67 @@
 import * as packageJson from "../package.json"
-//import * as cjp from "cjp"
+import merge from "deepmerge"
 
 interface SCInput {
-  target: string | string[]
+  target: string
   converter: SCConverter[]
 }
 interface SCConverter {
   name: string
-  option?: any | any[]
+  option?: any[]
+  status?: string
+  error?: unknown
 }
-
-const defaultConverter = {
-  cjp: require('cjp').generate
+interface SCOptions {
+  converter: {
+    [key: string]: Function
+  }
+}
+interface SCOutput {
+  text: string
+  result: SCConverter[]
 }
 
 class SC {
   version: string = packageJson.version
-
-  //constructor(option: {}) {}
-
-  test(): void {
-    console.log(defaultConverter.cjp("テスト"))
+  options: SCOptions = {
+    converter: {
+      cjp: require("cjp").generate,
+      genhera: require("genhera").generate,
+    },
   }
 
-  convert(input: SCInput): {} {
+  constructor(userOptions: SCOptions | {} = {}) {
+    const defaultOption = this.options
+    this.options = merge(defaultOption, userOptions)
+  }
+
+  convert(input: SCInput): SCOutput {
     if (typeof input !== "object" || input === null)
-      throw new TypeError("引数おかしいで")
+      throw new TypeError("The argument is unentered or not an object.")
     if (typeof input.target !== "string" && !Array.isArray(input.target))
-      throw new TypeError("変換対象おかしいで")
+      throw new TypeError("The target is unentered or is not a string.")
     if (!Array.isArray(input.converter))
-      throw new TypeError("変換指定おかしいで")
-
-    const output = {}
-
+      throw new TypeError("The converter is unentered or is not an object.")
+    const output: SCOutput = {
+      text: input.target,
+      result: input.converter,
+    }
+    for(const [index, val] of input.converter.entries()) {
+      const beforeText: string = output.text
+      try {
+        val.option || (val.option = [])
+        if (!Array.isArray(val.option))
+          throw new TypeError("The option is unentered or is not an array.")
+        output.text = this.options.converter[val.name](
+          ...[output.text, ...val.option]
+        )
+        output.result[index].status = "success"
+      } catch (error) {
+        output.text = beforeText
+        output.result[index].status = "error"
+        output.result[index].error = error
+      }
+    }
     return output
   }
 }
